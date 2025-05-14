5575cd633732a2ff1bcee3111bf1f5ea1e00d613
AdwaithAnandSR <adwaithanand1818@gmail.com>
2025-05-14 16:23:38 +0530

    f..

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ import { exec } from "child_process";
 const port = 3000;
 const app = express();
 
-import handleGetInfo from "./handlers/getInfo.js";
 import handleSetCookie from "./handlers/setCookie.js";
 
 import sanitizeUrl from "./utils/sanitizeUrl.js";
@@ -16,6 +15,25 @@ app.use(express.json());
 app.use(cors());
 
 app.post("/setCookie", handleSetCookie);
+
+app.post("/saveToCloud", (req, res) => {
+    let { data } = req.body;
+    
+    console.log(data, data[0]);
+
+    const [audioRes, thumbnailRes] = await Promise.all([
+        axios.get(data.url, { responseType: "arraybuffer" }),
+        axios.get(data.thumbnail, { responseType: "arraybuffer" })
+    ]);
+
+    const audioBuffer = Buffer.from(audioRes.data);
+    const thumbnailBuffer = Buffer.from(thumbnailRes.data);
+
+    // Log the buffers
+    console.log("Audio Buffer:", audioBuffer.length);
+    console.log("Thumbnail Buffer:", thumbnailBuffer.length);
+
+});
 
 app.post("/getInfo", (req, res) => {
     let { url } = req.body;
@@ -57,51 +75,5 @@ app.post("/getInfo", (req, res) => {
         }
     });
 });
-
-app.get("/info", async (req, res) => {
-    let { url } = req.query;
-
-    url = sanitizeUrl(url);
-
-    if (!url) return res.status(400).json({ error: "Missing YouTube URL" });
-
-    const cmd = `yt-dlp --cookies cookies.txt -j "${url}"`;
-
-    exec(cmd, (error, stdout, stderr) => {
-        if (error) {
-            console.error(stderr);
-            return res
-                .status(500)
-                .json({ error: "Failed to fetch info", detail: stderr });
-        }
-
-        try {
-            const info = JSON.parse(stdout);
-
-            res.json({
-                title: info.title,
-                uploader: info.uploader,
-                duration: info.duration,
-                thumbnail: info.thumbnail,
-                formats: info.formats,
-                audio_formats: info.formats
-                    .filter(f => f.asr && f.ext === "m4a")
-                    .map(f => ({
-                        quality: f.abr,
-                        format: f.format,
-                        ext: f.ext,
-                        url: f.url
-                    }))
-            });
-        } catch (err) {
-            res.status(500).json({
-                error: "JSON parse error",
-                detail: err.message
-            });
-        }
-    });
-});
-
-// https://www.youtube.com/watch?v=dQw4w9WgXcQ
 
 app.listen(port, () => console.log("app started"));
