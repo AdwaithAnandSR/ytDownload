baceb0d9f60fe3e61c6391b3ddd8aec6d6aee35a
AdwaithAnandSR <adwaithanand1818@gmail.com>
2025-05-14 13:43:12 +0530

    f..

--- server/handlers/getInfo.js
+++ server/handlers/getInfo.js
@@ -6,7 +6,7 @@ const handleGetInfo = async (req, res) => {
     let { url } = req.body;
 
     url = sanitizeUrl(url);
-
+    
     console.log(url);
 
     if (!url) return res.status(400).json({ error: "Missing YouTube URL" });
--- server/index.js
+++ server/index.js
@@ -7,20 +7,61 @@ const app = express();
 
 import handleGetInfo from "./handlers/getInfo.js";
 import handleSetCookie from "./handlers/setCookie.js";
-import sanitize from "./utils/sanitizeUrl.js";
+
+import sanitizeUrl from "./utils/sanitizeUrl.js";
+import sortFormates from "./utils/sortFormates.js";
+
 
 app.use(express.json());
 app.use(cors());
 
-
 app.post("/setCookie", handleSetCookie);
 
-app.post("/getInfo", handleGetInfo);
+app.post("/getInfo", (req, res) => {
+    let { url } = req.body;
+
+    url = sanitizeUrl(url);
+
+    console.log(url);
+
+    if (!url) return res.status(400).json({ error: "Missing YouTube URL" });
+
+    const cmd = `yt-dlp --cookies cookies.txt -j "${url}"`;
+
+    exec(cmd, (error, stdout, stderr) => {
+        if (error) {
+            console.error(stderr);
+            return res
+                .status(500)
+                .json({ error: "Failed to fetch info", detail: stderr });
+        }
+
+        try {
+            const info = JSON.parse(stdout);
+
+            const { audioQuality, videoQuality } = sortFormates(info.formats);
+
+            res.json({
+                title: info.title,
+                uploader: info.uploader,
+                duration: info.duration,
+                thumbnail: info.thumbnail,
+                audioQuality,
+                videoQuality
+            });
+        } catch (err) {
+            res.status(500).json({
+                error: "JSON parse error",
+                detail: err.message
+            });
+        }
+    });
+});
 
 app.get("/info", async (req, res) => {
     let { url } = req.query;
 
-    url = sanitize(url);
+    url = sanitizeUrl(url);
 
     if (!url) return res.status(400).json({ error: "Missing YouTube URL" });
 
