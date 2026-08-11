09a37e322d96e378b7df4ddbae81b1b315b5a2be
Adwaith Anand SR <181568233+AdwaithAnandSR@users.noreply.github.com>
2026-08-11 13:55:04 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,6 +15,12 @@ app.use(express.json());
 app.use(cors());
 
 import fs from "fs";
+// /etc/secrets
+
+const files = fs.readdirSync("/etc/secrets");
+
+console.log(files);
+
 
 app.get("/get-cookie", (req, res) => {
     const data = fs.readFileSync("./cookies.txt");
